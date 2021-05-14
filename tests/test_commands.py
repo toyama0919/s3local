a09d410b8748e8d37d33d04d086d90a1e4fbc9ce
Hiroshi Toyama <toyama0919@gmail.com>
2021-05-14 13:47:13 +0900

    add help test.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -13,3 +13,13 @@ def test_show_version(runner):
     result = runner.invoke(commands.cli, ["-v"])
     assert result.exit_code == 0
     assert result.output.strip() == constants.VERSION
+
+
+def test_show_help(runner):
+    result = runner.invoke(commands.cli, [])
+    assert result.exit_code == 0
+    assert "delete" in result.output.strip()
+    assert "list" in result.output.strip()
+    assert "list-local" in result.output.strip()
+    assert "upload" in result.output.strip()
+    assert "download" in result.output.strip()
