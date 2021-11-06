bdfad45625192beb7d3e2ca7b83501009e23579a
Hiroshi Toyama <toyama0919@gmail.com>
2021-11-06 14:17:09 +0900

    add init class.

--- src/s3local/__init__.py
+++ src/s3local/__init__.py
@@ -1,5 +1,15 @@
 from .core import Core
+from .downloader import Downloader
+from .uploader import Uploader
 
 
 class S3local(Core):
+    pass
+
+
+class Uploader(Uploader):
+    pass
+
+
+class Downloader(Downloader):
     pass
